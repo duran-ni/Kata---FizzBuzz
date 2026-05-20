579c35db0c835d2f3754ff53577fc6a88d3a56a2
duran-ni <duran_ni_7@hotmail.com>
2026-05-20 17:10:04 +0200

    feat: implement input validation to throw error on non-number data

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,24 @@
 export function fizzBuzz(number) {
-    // 1. Validar primero si es divisible por 3 y 5 al mismo tiempo (el caso del 15)
+    // 1. Validar si el dato proporcionado no es un número de forma estricta
+  if (typeof number !== 'number' || Number.isNaN(number)) {
+    throw new Error('The provided data is not a number');
+    
+  }
+  // 2. Validar si es divisible por 3 y 5 al mismo tiempo
   if (number % 3 === 0 && number % 5 === 0) {
     return 'FizzBuzz';
   }
-  // 2. Si no es quince, comprobar si es divisible por 3
+
+  // 3. Comprobar si es divisible por 3
   if (number % 3 === 0) {
     return 'Fizz';
   }
-  // 3. Comprobar si es divisible por 5
+
+  // 4. Comprobar si es divisible por 5
   if (number % 5 === 0) {
     return 'Buzz';
   }
-  // 4. Si no cumple ninguna de las anteriores, devolver el número como string
-  return String(number);
 
+  // 5. Devolver el número como string
+  return String(number);
 }
